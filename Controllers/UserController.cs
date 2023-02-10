c0e49a0cccfe0a720756222e22b18cc1d6198a10
hambalee <hambalee@outlook.com>
2023-02-10 14:49:52 +0700

    สร้างโมเดล User และอายุ

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,13 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
+using POC_NET7.Models;
 
 namespace POC_NET7.Controllers;
 
 [ApiController]
 [Route("api/users")]
-public class UserController : ControllerBase {
+public class UserController : ControllerBase
+{
 
     [HttpGet()]
-    public IActionResult GetUsers() {
-        return Ok(new { FirstName = "Lee"});
+    public IActionResult GetUsers()
+    {
+        User user = new User();
+        user.Firstname = "Lee";
+        user.Lastname = "Hambalee";
+        user.YearOfBirth = 1995;
+        user.Age = DateTime.UtcNow.Year - user.YearOfBirth;
+        return Ok(user);
     }
 }
---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+namespace POC_NET7.Models;
+
+public class User
+{
+    public int UserId { get; set; }
+    public string Firstname { get; set; } = "";
+    public string Lastname { get; set; } = "";
+    public int YearOfBirth { get; set; }
+    public int Age { get; set; }
+}
